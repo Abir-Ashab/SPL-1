8a278eba46184977522e232512b152b2f1157421
Abir Ashab Niloy <113261051+Abir-Ashab@users.noreply.github.com>
2023-04-08 02:07:58 +0600

    Add files via upload

--- DecisionTree.cpp
+++ DecisionTree.cpp
@@ -1,13 +1,134 @@
-#include<bits/stdc++.h>
 #include "decision_tree.h"
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <random>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
- 
+
+random_device rd;
+mt19937 gen(rd());
+
+vector<ll> k1, k2;
+
+// void RandomGenerator(int N)
+// {
+//     k1.push_back(N);
+//     for (int i = 0; i < 4; i++)
+//     {
+//         k1.push_back(rand() % N);
+//     }
+//     // cout << "K1" << '\n';
+//     // for( auto val : k1 ) cout << val << ' ';
+// }
+
+// void RandomGenerator2(int N)
+// {
+//     k2.push_back(0);
+//     N /= 2;
+//     for (int i = 0; i < 4; i++)
+//     {
+//         k2.push_back(rand() % N);
+//     }
+//     // cout << "K2" << '\n';
+//     // for( auto val : k2 ) cout << val << ' ';
+// }
+
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    if(DecisionTree(0)) cout << "YES, i will playtennis \n";
-    else cout << "NO, i won't playtennis\n";
+    // cout << "Number of files: ";
+    // int k, i, index = 0;
+    // cin >> k;
+
+    ll n, m;
+    cin >> n >> m;
+    vvs DATA(n);
+
+    // RandomGenerator(n);
+    // RandomGenerator2(n/2);
+
+    // int yes = 0;
+    // int no = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            string s;
+            cin >> s;
+            DATA[i].push_back(s);
+        }
+    }
+    map<string, string> mq, mq2;
+    for (int j = 0; j < m - 1; j++)
+    {
+        string s;
+        cin >> s;
+        mq[DATA[0][j]] = s;
+    }
+    // for (int k = 0; k < 5; ++k)
+    // {
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << DATA[i][j] << "       ";
+        }
+        cout << '\n';
+    }
+    // initial attr_vals;
+
+    for (int j = 0; j < m - 1; j++)
+    {
+        map<string, bool> mp;
+        for (int i = 1 ; i < n; i++)
+        {
+            if (!mp[DATA[i][j]])
+            {
+                mp[DATA[i][j]] = 1;
+                attr_vals[DATA[0][j]].push_back(DATA[i][j]);
+            }
+        }
+    }
+
+    node *root = new node();
+    func(root, DATA);
+    // int question;
+    // cin >> question;
+
+    // for (int i = 0; i < question; i++)
+    // {
+    // cout << "\n\nFor the following data : ";
+
+    for (int j = 0; j < m - 1; j++)
+    {
+        string s;
+        s = mq[DATA[0][j]];
+        // cout << s << "  ";
+        mq2[DATA[0][j]] = s;
+    }
+
+    // cout << "\n\nMy decision is : ";
+
+    if (decision_function(root, mq2))
+    {
+        yes++;
+    }
+    else
+    {
+        no++;
+    }
+    //}
+    if (yes > no)
+    {
+        cout << "Yes,I will play tennis\n";
+    }
+    else
+    {
+        cout << "No, I won't play tennis\n";
+    }
 }
